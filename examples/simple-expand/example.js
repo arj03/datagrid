9d37f57d8643b00003c3903fb344421733a076e0
Anders Rune Jensen <anders@iola.dk>
2014-05-31 22:26:24 +0200

    Fix sorting

--- examples/simple-expand/example.js
+++ examples/simple-expand/example.js
@@ -1,7 +1,7 @@
 $(function() {
 
     function cellValue(val) {
-        return { sortValue: val, displayValue: val };
+        return { sortValue: val, displayValue: val.toString() };
     }
 
     var yAxis = [{ id: 'store', name: 'Store' }, { id: 'clerk', name: 'Clerk' }];
@@ -15,10 +15,10 @@ $(function() {
     reportState.dimensionsY = _.map(yAxis, function(e) { return e.id; });
 
     reportState.serverData = [
-        { type: 'row', values: [cellValue('Copenhagen'), cellValue(''), cellValue("210"), cellValue("43100")] },
-        { type: 'row', values: [cellValue('Stockholm'), cellValue(''), cellValue("120"), cellValue("22100")] },
-        { type: 'row', values: [cellValue('Berlin'), cellValue(''), cellValue("743"), cellValue("50032")] },
-        { type: 'grandtotal', values: [cellValue('Grand total'), cellValue(''), cellValue("1073"), cellValue("115232")] }
+        { type: 'row', values: [cellValue('Copenhagen'), cellValue(''), cellValue(210), cellValue(43100)] },
+        { type: 'row', values: [cellValue('Stockholm'), cellValue(''), cellValue(120), cellValue(22100)] },
+        { type: 'row', values: [cellValue('Berlin'), cellValue(''), cellValue(743), cellValue(50032)] },
+        { type: 'grandtotal', values: [cellValue('Grand total'), cellValue(''), cellValue(1073), cellValue(115232)] }
     ];
 
     var allYAxisValues = reportBuilder.getYAxisValues(reportState.serverData, yAxis);
@@ -29,35 +29,35 @@ $(function() {
 
         if (_.any(reportState.expandedCells['store'], function(e) { return e == 'store:Copenhagen'; }))
         {
-            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue('Stine'), cellValue("110"), cellValue("33100")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue('Dorthe'), cellValue("100"), cellValue("10000")] });
-            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Copenhagen'), cellValue(''), cellValue("210"), cellValue("43100")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue('Stine'), cellValue(110), cellValue(33100)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue('Dorthe'), cellValue(100), cellValue(10000)] });
+            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Copenhagen'), cellValue(''), cellValue(210), cellValue(43100)] });
         }
         else
-            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue(''), cellValue("210"), cellValue("43100")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Copenhagen'), cellValue(''), cellValue(210), cellValue(43100)] });
 
         if (_.any(reportState.expandedCells['store'], function(e) { return e == 'store:Stockholm'; }))
         {
-            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Emma'), cellValue("30"), cellValue("2100")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Anne'), cellValue("70"), cellValue("18000")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Julia'), cellValue("20"), cellValue("2000")] });
-            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Stockholm'), cellValue(''), cellValue("120"), cellValue("22100")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Emma'), cellValue(30), cellValue(2100)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Anne'), cellValue(70), cellValue(18000)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue('Julia'), cellValue(20), cellValue(2000)] });
+            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Stockholm'), cellValue(''), cellValue(120), cellValue(22100)] });
         }
         else
-            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue(''), cellValue("120"), cellValue("22100")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Stockholm'), cellValue(''), cellValue(120), cellValue(22100)] });
 
         if (_.any(reportState.expandedCells['store'], function(e) { return e == 'store:Berlin'; }))
         {
-            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Sandra'), cellValue("93"), cellValue("1182")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Katharina'), cellValue("100"), cellValue("6700")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Nadine'), cellValue("120"), cellValue("10030")] });
-            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Julia'), cellValue("430"), cellValue("30200")] });
-            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Berlin'), cellValue(''), cellValue("743"), cellValue("50032")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Sandra'), cellValue(93), cellValue(1182)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Katharina'), cellValue(100), cellValue(6700)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Nadine'), cellValue(120), cellValue(10030)] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue('Julia'), cellValue(430), cellValue(30200)] });
+            reportState.serverData.push({ type: 'subtotal', values: [cellValue('Berlin'), cellValue(''), cellValue(743), cellValue(50032)] });
         }
         else
-            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue(''), cellValue("743"), cellValue("50032")] });
+            reportState.serverData.push({ type: 'row', values: [cellValue('Berlin'), cellValue(''), cellValue(743), cellValue(50032)] });
 
-        reportState.serverData.push({ type: 'grandtotal', values: [cellValue('Grand total'), cellValue(''), cellValue("1073"), cellValue("115232")] });
+        reportState.serverData.push({ type: 'grandtotal', values: [cellValue('Grand total'), cellValue(''), cellValue(1073), cellValue(115232)] });
 
         if (reportState.sortRowIndex != -1)
             reportState.drawData(reportBuilder.sortExpandedData(reportState.serverData, reportState.dimensionsY, reportState.sortRowIndex, reportState.sortDirection, reportState.expandedCells));
--- examples/simple-sort/example.js
+++ examples/simple-sort/example.js
@@ -1,7 +1,7 @@
 $(function() {
 
     function cellValue(val) {
-        return { sortValue: val, displayValue: val };
+        return { sortValue: val, displayValue: val.toString() };
     }
 
     var yAxis = [{ id: 'store', name: 'Store' }];
@@ -13,10 +13,10 @@ $(function() {
     var reportState = new ReportState();
 
     reportState.serverData = [
-        { type: 'row', values: [cellValue('Copenhagen'), cellValue("210"), cellValue("43100")] },
-        { type: 'row', values: [cellValue('Stockholm'), cellValue("120"), cellValue("22100")] },
-        { type: 'row', values: [cellValue('Berlin'), cellValue("743"), cellValue("50032")] },
-        { type: 'grandtotal', values: [cellValue('Grand total'), cellValue("1073"), cellValue("115232")] }
+        { type: 'row', values: [cellValue('Copenhagen'), cellValue(210), cellValue(43100)] },
+        { type: 'row', values: [cellValue('Stockholm'), cellValue(120), cellValue(22100)] },
+        { type: 'row', values: [cellValue('Berlin'), cellValue(743), cellValue(50032)] },
+        { type: 'grandtotal', values: [cellValue('Grand total'), cellValue(1073), cellValue(115232)] }
     ];
     
     var allYAxisValues = reportBuilder.getYAxisValues(reportState.serverData, yAxis);
